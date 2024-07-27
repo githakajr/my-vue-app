f4974096089886d663ac063bc32ba08dced26830
githakajr <githakajr@gmail.com>
2024-07-27 13:44:11 +0300

    Initial commit

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="my-component">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyComponent',
+  data() {
+    return {
+      message: 'Single-File Components In Vue.js'
+    };
+  }
+};
+</script>
+
+<style scoped>
+.my-component {
+  text-align: center;
+  color: #42b983;
+}
+</style>
